6d78a9ea2002019abcc282ccd606ba2cd26b6cab
Andriy Romanyuk <vandzhuraandrew@gmail.com>
2021-11-04 15:03:31 +0200

    FALLALAL

--- Lab 6/happy.py
+++ Lab 6/happy.py
@@ -45,3 +45,5 @@ def happy_numbers(first_ticket: int, last_ticket: int) -> list:
             happy_numbers_lst.append(first_ticket)
         first_ticket += 1
     return happy_numbers_lst
+
+print("PUB")
